bcaef62083efcae2c600dc0e742a5ad49dc6cbc4
Kunal Kumar Rajak <kunalrajak979@gmail.com>
2026-03-28 14:01:21 +0530

    Second Commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://tawangmemories.deployhub.online'],
+    methods: ['POST' , 'GET' , 'PUT' , 'DELETE' , 'OPTIONS' , 'PATCH'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization' , 'x-admin-password']
+}));
 app.use(express.json());
 
 // Routes
